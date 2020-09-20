428fb616f97c6e8ba7427c19ab7c1e1bc0238674
GarenWang <2270099149@qq.com>
2020-09-20 11:34:51 +0800

    显示帖子

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,18 +1,27 @@
 <template>
-  <div>
+  <div v-for="(comment, index) in comments" :key="index">
+    <div>{{comment.username}}: {{comment.content}}</div>
     <CommentItem></CommentItem>
   </div>
 </template>
 
 <script>
 import CommentItem from "./CommentItem";
+import { ref } from "vue";
 export default {
   name: "CommentList",
   components: {
     CommentItem
   },
   setup() {
-    return {};
+    const comments = ref([
+      { username: "张三", content: "今天天气不错" },
+      { username: "李四", content: "天气有点热" },
+      { username: "王五", content: "要一起出去玩吗" }
+    ]);
+    return {
+      comments
+    };
   }
 };
 </script>
